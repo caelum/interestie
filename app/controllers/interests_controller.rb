6fa2f9c4fd20f332ac67a88b71873af48eff49f8
guilherme e erich <guilherme.silveira@caelum.com.br>
2012-05-21 08:00:48 -0300

    refatorando pra ficar mais bonito

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,10 +1,17 @@
 class InterestsController < ApplicationController
+  
   def create
     interest = Interest.create params[:interest]
-    if interest.valid?
-      render text: "#{params[:callback]}({ ok : 'ok'});"
-    else
-      render text: "#{params[:callback]}({ errors : 'unable to set up interest' });"
-    end
+    msg = interest.valid? ? ok_msg : error_msg
+    
+    render :json => msg, :callback => params[:callback]
+  end
+  
+  private
+  def error_msg
+    { 'errors' => ['unable to set up interest'] }
+  end
+  def ok_msg
+    { 'ok' => 'ok'}
   end
 end
--- test/functional/interests_controller_test.rb
+++ test/functional/interests_controller_test.rb
@@ -3,14 +3,19 @@ require 'test_helper'
 class InterestsControllerTest < ActionController::TestCase
   test "should create an interest" do
     get :create, {interest: {email: "guilherme.silveira@caelum.com.br", item: "fj-11"}, callback: 'cb'}
-    assert_equal response.body,  "cb({ ok : 'ok'});"
+    assert_body  'cb({"ok":"ok"})'
   end
 
   test "should complain if missing an email or item" do
     get :create, {interest: {email: "guilherme.silveira@caelum.com.br"}, callback: 'cb'}
-    assert_equal response.body,  "cb({ errors : 'unable to set up interest' });"
+    assert_body 'cb({"errors":["unable to set up interest"]})'
     get :create, {interest: {item: "fj-11"}, callback: 'cb'}
-    assert_equal response.body,  "cb({ errors : 'unable to set up interest' });"
+    assert_body 'cb({"errors":["unable to set up interest"]})'
+  end
+  
+  private
+  def assert_body(expected)
+    assert_equal response.body, expected
   end
 
 end
