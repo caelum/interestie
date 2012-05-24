178a8a1209ea01bb1502f486d357428bbc029a4a
guilherme e erich <guilherme.silveira@caelum.com.br>
2012-05-24 08:10:13 -0300

    ranked by interest

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -2,4 +2,8 @@ class Interest < ActiveRecord::Base
   attr_accessible :email, :item
   validates :item, presence: true
   validates :email, length: { :minimum => 5 }
+  
+  def self.rank
+    Hash[Interest.all.group_by(&:item).map{|k,v| [k, v.length]}]
+  end
 end
