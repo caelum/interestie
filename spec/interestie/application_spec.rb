93b073620bbf6ebd31db31e2725117e8968470ac
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2012-07-13 14:51:21 -0300

    requiring rspec in the spec_helper now :clap:

--- spec/interestie/application_spec.rb
+++ spec/interestie/application_spec.rb
@@ -1,4 +1,3 @@
-require 'rspec'
 require 'rack/test'
 
 set :environment, :test
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
-$LOAD_PATH.unshift File.expand_path('../../lib/', __FILE__)
+require 'rspec'
 
+$LOAD_PATH.unshift File.expand_path('../../lib/', __FILE__)
 require "interestie"
-original_env = ENV["RACK_ENV"]
+
 ENV["RACK_ENV"] = "test"
 Interestie::Connection.establish
 
